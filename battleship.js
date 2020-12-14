df8e771c35586b5172211cc058a5a813212426b0
Swdenis <swdenis@mail.ru>
2020-12-14 18:24:31 +0100

    added random functionality

--- battleship.js
+++ battleship.js
@@ -1,10 +1,12 @@
-var location1=3;
-var location2=4;
-var location3=5;
+var randomLoc = Math.floor(Math.random()*5);
+var location1=randomLoc;
+var location2=location1+1;
+var location3=location2+1;
 var guess;
 var hits=0;
 var guesses=0;
 var isSunk=false;
+
 
 while (isSunk==false) {
 guess= prompt("Ready, aim, fire! (Enter a number 0-6):");
